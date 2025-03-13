a1c97376087b274d2993bb3799c4b9e69b6460c2
Angelika Brown <angelikpbrown@gmail.com>
2025-03-13 19:22:48 -0400

    Update roll.py

--- roll.py
+++ roll.py
@@ -1,29 +1,21 @@
 from random import randrange
 
-# roll a "die" some number of times.
-# rolling die 3x
-#result = ""
-
-# roll - run it once and go into a loop
-
-
-
 def die():
 	roll = randrange(1,7)
 	return roll
 
 def game():
 	play_game = True
-while True:
-	rolls[] = {}
-	x = input("Your first roll is: " + randrange(1,7))	
-	rolls.append(x)
-for i in range int(x):
-	d = input("Your next roll is: " )
-	rolls.append(d)
-p = input("Would you like to roll again?")
-	if p == 'N':
-		play_game = False
+	while True:
+		rolls = []
+		x = input("Your first roll is: " + randrange(1, 7))
+		rolls.append(x)
+		for i in range x:
+			d = input("Your next roll is: " )
+			rolls.append(d)
+		p = input("Would you like to roll again?")
+		if p == 'N':
+			play_game = False
 
-play_game()
 
+game()
