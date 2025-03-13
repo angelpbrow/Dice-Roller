bfb81f799add58be9e4a002db122bd4e6246c172
Angelika Brown <angelikpbrown@gmail.com>
2025-03-13 16:36:12 -0400

    rolling the code dice

--- roll.py
+++ roll.py
@@ -1,28 +1,33 @@
 from random import randrange
 
-# roll a "die" some number of times.
-# rolling die 3x
-x = 3
+rolls = []
 
-# roll - run it once and go into a loop
-  def die(n):
-	roll = randrange(6)
-	print (roll * n) 
-#can the print be multiplied by n roll once it's rolled again?
+def die():
+	roll1 = randrange(6)
+	return rolls.append(roll1)
 
-while x >= 3:
-# roll 1 - produce a number 1-6 random and print it
- if n == 1:
-	die(1) --> will print (roll * 1)
-# roll 2 - produce two numbers 1-6 and print them
-elif n == 2: 
-  die(2) --> will print (roll * 2)
-    print(die(2) = roll2 die(), roll1 die())
-# roll 3 - produce three numbers and print them.
-elif n == 3
-   die(3) --> will print (roll * 3)
-      print(die(3))= roll1, roll2, roll3)
-#
-else:
-print("youve ran out of rolls")
+def die2():
+	roll2 = randrange(6)
+	return rolls.append(roll2)
 
+def die3():
+	roll3 = randrange(6)
+	return rolls.append(roll3)
+
+
+i = 0
+def play():
+
+    while (i <= 3):
+        if (i == 1):
+	         return die()
+		elif (i == 2):
+			return die2()
+        else:
+			return die3()
+        i += 1
+
+#die()
+#die2()
+#die3()
+print(rolls)
