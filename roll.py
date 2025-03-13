f384eea75ae51d2591726173003e658672a5798a
Angelika Brown <angelikpbrown@gmail.com>
2025-03-12 21:54:29 -0400

    pseduocoding

--- roll.py
+++ roll.py
@@ -1,7 +1,20 @@
+from random import randrange
+
 # roll a "die" some number of times.
+# rolling die 3x
+x = 3
+while x >= 3:
 # roll - run it once and go into a loop
+  
+  
 # roll 1 - produce a number 1-6 random and print it
+ roll1 = randrange(6)
+  print(roll1)
 # roll 2 - produce two numbers 1-6 and print them
+  roll2 = randrange(6)
+    print(roll1, roll2)
 # roll 3 - produce three numbers and print them.
+    roll3 = randrange(6)
+      print(roll1, roll2, roll3)
 #
 
